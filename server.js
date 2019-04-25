9e5d509ea470ea0f671ebb9354ba33d7836aea02
jerryhuang3 <jerryhuang@utexas.edu>
2019-04-25 17:41:06 -0400

    changed port

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const app = express();
 app.use(express.static('dist'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/dist/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
