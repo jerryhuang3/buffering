df8680944615480fe576b3da614bfba9c9136e92
Matt <Matt@CMB.local>
2019-04-26 13:27:46 -0400

    Fix exports.down to actually delete type on rollback

--- db/migrations/20190426160005_standing.js
+++ db/migrations/20190426160005_standing.js
@@ -15,15 +15,22 @@ exports.up = function (knex, Promise) {
         })
         .createTable('standing', function (table) {
             table.integer('day');
-            table.enu('status', ['bad', 'awful', 'hell'], { useNative: true, enumName: 'standing_type' });
+            table.enu('status', ['good', 'bad', 'awful', 'hell'], { useNative: true, enumName: 'standing_type' });
             table.integer('user_id');
             table.foreign('user_id').references('users.id');
         })
 };
 
 exports.down = function (knex, Promise) {
-    return knex.schema
-        .dropTable('goals')
-        .dropTable('progress')
-        .dropTable('standing')
-};
+    return Promise.all([
+        knex.schema
+            .dropTable('goals')
+            .dropTable('progress')
+            .dropTable('standing')
+    ])
+        .then(function () {
+            return Promise.all([
+                knex.raw("DROP TYPE standing_type")
+            ])
+        })
+}
