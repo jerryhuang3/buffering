b721b907cbcf3bb56606417eaa5b1d598be9a077
jerryhuang3 <jerryhuang@utexas.edu>
2019-04-26 12:22:43 -0400

    port: 3000; title: set to 'Buffering'

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path'); 
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
 
 
