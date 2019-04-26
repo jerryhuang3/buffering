1cd57d31b5cb53d1100d91ec17dd2ee9da123fed
Aleksey Van Leeuwarden <Leeuwarden3@gmail.com>
2019-04-26 23:30:50 +0000

    test query working and server connected to db

--- db/queries.js
+++ db/queries.js
@@ -1,3 +1,44 @@
+require('dotenv').config();
 // import knex
-const knexConfig  = require("./knexfile");
-const knex        = require("knex")(knexConfig[ENV]);
+const knexConfig  = require("../knexfile");
+const knex        = require("knex")(knexConfig.development);
+
+function testIsWorking () {
+  return Promise.all([
+    knex.select().from('users')
+    ]);
+}
+
+module.exports = {
+  testIsWorking: testIsWorking
+}
+
+// DATABASE STRUCTURE
+// ==================
+
+// USERS
+// -----------
+// id
+// username
+// email
+// password_digest
+// wallet_amount
+// token
+
+// GOALS
+// -----------
+// day
+// steps_goal
+// user_id
+
+// PROGRESS
+// -----------
+// day
+// steps_walked
+// user_id
+
+// STANDING
+// -----------
+// day
+// status  -> enum = standing_type ; can be one of [good, bad, awful, hell] (all lowercase!)
+// user_id
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const ENV         = process.env.ENV || "development";
 // import express and related libraries
 const express     = require("express");
 const bodyParser  = require("body-parser");
-const sass        = require("node-sass-middleware");
+//const sass        = require("node-sass-middleware");
 const path        = require('path');
 //import helpers
 const queries     = require('./db/queries');
@@ -20,6 +20,8 @@ app.use('/', express.static(path.join(__dirname, 'dist')));
 
 
 
-app.listen(port, () =>
-    console.log(`Server listening on ${port}`)
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`)
+    queries.testIsWorking().then( result => console.log(result));
+  }
 );
