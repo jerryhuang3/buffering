97e5d9723f14b0b64d49535a6bdbdae02cb7aae2
Matt <Matt@CMB.local>
2019-04-27 16:16:14 -0400

    Add test XHR request from extension background to server

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const express     = require("express");
 const bodyParser  = require("body-parser");
 //const sass        = require("node-sass-middleware");
 const path        = require('path');
+//allow cross origin resource sharing
+const cors        = require("cors");
 //import helpers
 const queries     = require('./db/queries');
 
@@ -18,11 +20,12 @@ const queries     = require('./db/queries');
 const app = express();
 app.use('/', express.static(path.join(__dirname, 'dist')));
 
-
+app.get("/test", cors(), (req, res) =>
+  res.json({ message: "Hello From the Server!" })
+);
 
 app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`)
-    queries.testIsWorking().then( result => console.log(result))
-    queries.getUserProfile("Good").then( result => console.log(result));
-  }
-);
+  console.log(`Server listening on ${PORT}`);
+  queries.testIsWorking().then(result => console.log(result));
+  queries.getUserProfile("Good").then(result => console.log(result));
+});
