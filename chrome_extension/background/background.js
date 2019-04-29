ecf2ab64281a95f4352f8ea9536bcba227be2abd
MattTurnip <mattadavey@gmail.com>
2019-04-29 22:42:06 +0000

    Add extension ability to inject scripts/css based on message requested from server

--- chrome_extension/background/background.js
+++ chrome_extension/background/background.js
@@ -1,10 +1,9 @@
-
 // //test http request to server
 // var xhttp = new XMLHttpRequest();
 
 // xhttp.onreadystatechange = function() {
 //   if (this.readyState == 4 && this.status == 200) {
-    
+
 //     const response = xhttp.responseText
 //     const status = xhttp.status
 //     const statusText = xhttp.statusText
@@ -20,11 +19,22 @@
 // console.log("before async request")
 
 // // receive message from content script.
-chrome.runtime.onMessage.addListener(function(response, sender, sendResponse){
-   console.log(response)
-   chrome.tabs.executeScript({file: "../css/test.css"},
-   function(results){ console.log(results); } );
-})
+// chrome.runtime.onMessage.addListener(function(response, sender, sendResponse) {
+//   console.log(response);
+//   chrome.tabs.insertCSS(null, { file: "../css/test.css" }, function(results) {
+//     console.log(results);
+//   });
+// });
 
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  console.log(
+    sender.tab
+      ? request.greeting + " from a content script: " + sender.tab.url
+      : "this shouldn't happen"
+  );
+  if (request.greeting == "hello") {
+    chrome.tabs.insertCSS(null, { file: "./css/test.css" });
 
-
+    sendResponse({ farewell: "goodbye" });
+  }
+});
