e7c0f1d9e3fe1df4f80fbd12060e7327857834f8
Aleksey Van Leeuwarden <Leeuwarden3@gmail.com>
2019-05-02 23:01:35 +0000

    goal seeds work

--- db/seeds/goal_times.js
+++ db/seeds/goal_times.js
@@ -8,76 +8,40 @@ for (let i = 1; i < 7; i++) {
   pastWeekArray.push(ithDayAgo);
 }
 
-// exports.seed = function(knex, Promise) {
-//   // Deletes ALL existing entries
-//   return Promise.all([
-//     knex('goals')
-//       .del()
-//       .then(function() {
-//         // Inserts seed entries
-//         return Promise.all([
-//           knex('google_users')
-//             .where('id', '=', 5)
-//             .select()
-//             .then(usersArray => {
-//               usersArray.forEach(user => {
-//                 console.log(user);
-//                 const googleId = user.google_id;
-//                 pastWeekArray.forEach(dayRounded => {
-//                   console.log(dayRounded);
-//                   const randomNum = 1000 + Math.floor(3000 * Math.random());
-//                   return Promise.all([
-//                     knex('goals').insert({
-//                       google_id: googleId,
-//                       steps_goal: randomNum,
-//                       day_rounded: dayRounded
-//                     })
-//                   ]);
-//                 });
-//               });
-//             })
-//         ]);
-//       })
-//   ]);
-// };
-const paramsArray = [];
-exports.seed = function(knex, Promise) {
+
+
+
+exports.seed = async function(knex, Promise) {
   // Deletes ALL existing entries
-  return Promise.all([
-    knex('goals')
-      .del()
-      .then(function() {
-        // Inserts seed entries
-        return Promise.all([
-          knex('google_users')
-            .where('id', '=', 5)
-            .select()
-            .then(usersArray => {
-              usersArray.forEach(user => {
-                pastWeekArray.forEach(dayRounded => {
-                  const obj = {
-                    google_id: user.google_id,
-                    steps_goal: (1000 + Math.floor(3000 * Math.random())),
-                    day_rounded: dayRounded
-                  };
-                  console.log(obj);
-                  paramsArray.push(obj);
-                });
-              });
-            })
-            .then(() => {
-              return paramsArray.map(obj => {
-                knex('goals').insert({
-                  google_id: obj.google_id,
-                  steps_goal: obj.steps_goal,
-                  day_rounded: obj.day_rounded
-                });
-              });
-            })
-            .then(seed => {
-              return Promise.all(seed);
-            })
-        ]);
+  await knex('goals').del();
+  let paramsArray = [];
+
+  const currentUsers = await knex('google_users').select()
+  console.log(currentUsers);
+  const userArray = currentUsers.map( userObj => userObj.google_id);
+  console.log(userArray);
+  userArray.forEach( userId => {
+    pastWeekArray.forEach( dayRounded => {
+      const obj = {
+        google_id: userId,
+        steps_goal: (1000 + Math.floor(3000 * Math.random())),
+        day_rounded: dayRounded
+      }
+      paramsArray.push(obj);
+    })
+  })
+
+  console.log(paramsArray);
+
+  const insertsToRun = paramsArray.map( obj => {
+    return knex('goals')
+      .insert({
+        google_id: obj.google_id,
+        steps_goal: obj.steps_goal,
+        day_rounded: obj.day_rounded
       })
-  ]);
+  })
+
+  console.log(insertsToRun)
+  return Promise.all(insertsToRun);
 };
