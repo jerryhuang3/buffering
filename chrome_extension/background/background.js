1bf87c89174cc76843fcfc661df380a9f794d6aa
Matt <Matt@CMB.local>
2019-05-02 20:55:46 -0400

    Add css injects, scripts

--- chrome_extension/background/background.js
+++ chrome_extension/background/background.js
@@ -1,3 +1,25 @@
+const hellScripts = ['./scriptmods/mouseMoveVisibility.js'];
+const awfulScripts = ['./scriptmods/geo.js', './scriptmods/image.js'];
+const badScripts = [];
+
+function randomifyScript(status) {
+  if (status === 'hell') {
+    return hellScripts[Math.floor(Math.random() * hellScripts.length)];
+  }
+  if (status === 'awful') {
+    return awfulScripts[Math.floor(Math.random() * awfulScripts.length)];
+  }
+  if (status === 'bad') {
+    return badScripts[Math.floor(Math.random() * badScripts.length)];
+  }
+  //the following if block cab be removed it is for testing only.
+  if (status === 'hello') {
+    const testArray = [1, 2, 3, 4, 5, 6, 7, 8];
+    console.log('testing a random awful script based on hello msg');
+    return console.log(testArray[Math.floor(Math.random() * testArray.length)]);
+  }
+}
+
 //listener for messages from content script/ executes scripts based on message
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   console.log(
@@ -7,12 +29,17 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   );
   if (request.greeting == 'hello') {
     //insert scripts to execute here
-    chrome.tabs.executeScript(null, { file: './scriptmods/image.js' });
+    randomifyScript(request.greeting);
+    chrome.tabs.executeScript(null, { file: './scriptmods/scrollToBottom.js' });
+    chrome.tabs.insertCSS(null, { file: './css/pulsate.css' });
+    chrome.tabs.executeScript(null, { file: './scriptmods/geo.js' });
+
     sendResponse({ farewell: 'goodbye' });
   }
 });
 
 //some scripts/css to inject
-// chrome.tabs.insertCSS(null, { file: './css/test.css' });
+// chrome.tabs.insertCSS(null, { file: './css/spin.css' });
 // chrome.tabs.executeScript(null, { file: './scriptmods/mouseMoveVisibility.js' });
 // chrome.tabs.executeScript(null, { file: './scriptmods/geo.js' });
+// chrome.tabs.executeScript(null, { file: './scriptmods/image.js' });
--- chrome_extension/scriptmods/mouseMoveVisibility.js
+++ chrome_extension/scriptmods/mouseMoveVisibility.js
@@ -1,8 +1,10 @@
-$('body').mousemove(function(event) {
-  $('*').css('visibility', 'hidden');
-  $('head').append($('<style>* {cursor : help }</style>'));
-  setTimeout(function() {
-    $('*').css('visibility', 'visible');
-    $('head').append($('<style>* {cursor : auto }</style>'));
-  }, 2000);
+$(document).ready(function() {
+  $('body').mousemove(function(event) {
+    $('*').css('visibility', 'hidden');
+    $('head').append($('<style>* {cursor : help }</style>'));
+    setTimeout(function() {
+      $('*').css('visibility', 'visible');
+      $('head').append($('<style>* {cursor : auto }</style>'));
+    }, 2000);
+  });
 });
